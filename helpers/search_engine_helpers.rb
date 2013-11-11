094ae0cab36792819457f434c4c545ba706c885a
Derek Conjar <derek@newadventuremedia.com>
2013-11-11 14:50:28 -0500

    Fix social button links

--- helpers/search_engine_helpers.rb
+++ helpers/search_engine_helpers.rb
@@ -1,14 +1,22 @@
 module SearchEngineHelpers
-    def title_helper
-        current_page.data.title || data.site.name
+    def title_helper(article = nil)
+        if article.nil?
+            escape_html(current_page.data.title || data.site.name)
+        else
+            escape_html(article.title)
+        end
     end
 
     def description_helper
         current_page.data.description || data.site.description
     end
 
-    def permalink_helper
-        data.site.url + current_page.url.gsub("index.html", "")
+    def permalink_helper(article = nil)
+        if article.nil?
+            escape_html(data.site.url + current_page.url.gsub("index.html", ""))
+        else
+            escape_html("#{data.site.url}/#{article.slug}/")
+        end
     end
 
     def image_helper
