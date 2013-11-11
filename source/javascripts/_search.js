3baeb9ae0a85d195d6a97e8a7afc5a07b0f2c41a
Derek Conjar <derek@derekconjar.com>
2013-11-10 23:03:53 -0500

    Add site search

---
+++ source/javascripts/_search.js
@@ -0,0 +1,8 @@
+// When people click the search button, load the Google search box
+
+var loadSearchBox = function () {
+    $('.search-button').click(function () {
+        $('.search-box').toggle(1000)
+        $('.search-box input[type=text').focus()
+    })
+}
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -16,6 +16,7 @@
 $(document).ready(function(){
     fixSidebarHeight()
     addScrollingCodeBlocks()
+    loadSearchBox()
     enableDisqusComments()
 })
 
