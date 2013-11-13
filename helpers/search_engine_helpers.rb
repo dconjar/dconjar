b70b7ee06ef840a36628d6625958df7ce116134f
Derek Conjar <derek@newadventuremedia.com>
2013-11-13 16:50:53 -0500

    Improve search engine helpers

--- helpers/search_engine_helpers.rb
+++ helpers/search_engine_helpers.rb
@@ -1,26 +1,30 @@
 module SearchEngineHelpers
     def title_helper(article = nil)
         if article.nil?
-            escape_html(current_page.data.title || data.site.name)
+            if current_page.data.title.nil?
+                data.site.name
+            else
+                escape_html(current_page.data.title)
+            end
         else
             escape_html(article.title)
         end
     end
 
     def description_helper
-        current_page.data.description || data.site.description
+        escape_html(current_page.data.description.to_s) || escape_html(data.site.description)
     end
 
     def permalink_helper(article = nil)
         if article.nil?
-            escape_html(data.site.url + current_page.url.gsub("index.html", ""))
+            data.site.url + current_page.url.gsub("index.html", "")
         else
-            escape_html("#{data.site.url}#{article.url}")
+            "#{data.site.url}#{article.url}"
         end
     end
 
     def image_helper
-        current_page.data.image || nil
+        data.site.url + '/images/' + current_page.data.image.to_s || nil
     end
 
     def rss_link_helper
